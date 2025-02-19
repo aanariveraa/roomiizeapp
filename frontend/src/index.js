efb6020702861b4d07945cadfc8dc96cc89e1c2d
andrea-s7 <as.andreaserrano@gmail.com>
2025-02-19 11:03:06 -0800

    basic signup+login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app';
+import './rooms/rooms.css';
+import Rooms3d from './rooms/rooms3d';
+import App from './app.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
