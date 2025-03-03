13b8be5254246375b9c13fd702e4be2d1212d7fe
Bryan Barrios <bryanbarrios@macbookair.lan>
2025-03-02 21:53:44 -0800

    Fixed dependency issues and updated project setup

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -5,6 +5,7 @@ import { Button } from "react-bootstrap";
 import GoogleButton from "react-google-button";
 import { useUserAuth } from "../context/UserAuthContext";
 import Rooms3d from '../rooms/rooms3d';
+import "./Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -37,49 +38,56 @@ const Login = () => {
 
   return (
     <>
-      <div className="p-4 box">
-        <h2 className="mb-3">Firebase/ React Auth Login</h2>
+    <div id="main-wrapper">
+    <div className="p-4 box">
+      <h2 className="mb-3">Roomiize</h2>
 
-        {error && <Alert variant="danger">{error}</Alert>}
+      {error && <Alert variant="danger">{error}</Alert>}
 
-        <Form onSubmit={handleSubmit}>
-          
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Email address"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
+      <Form onSubmit={handleSubmit}>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-
-          <div className="d-grid gap-2">
-            <Button variant="primary" type="Submit">
-              Log In
-            </Button>
-          </div>
-        </Form>
-        <hr />
-        <div>
-          <GoogleButton
-            className="g-btn"
-            type="dark"
-            onClick={handleGoogleSignIn}
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control
+            type="email"
+            placeholder="Email Address"
+            onChange={(e) => setEmail(e.target.value)}
           />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <div className="d-grid gap-2">
+          <Button variant="primary" type="Submit">
+            Log In
+          </Button>
         </div>
+      </Form>
+      <hr />
+      <div>
+        <GoogleButton
+          className="g-btn"
+          type="dark"
+          onClick={handleGoogleSignIn}
+        />
       </div>
-      <div className="p-4 box mt-3 text-center">
-        Don't have an account? <Link to="/signup">Sign up</Link>
+      <div>
+      Don't have an account? <Link to="/signup">Sign up</Link>
+    </div>
+    </div>
+
+    <div className="rs">
+      {/* <img src= {'https://blenderartists.org/uploads/default/original/4X/a/6/1/a61d064bb4948be440e86580f3d659f77a37989b.jpeg'%7D/> */}
+
+      <img src= {'https://www.blendernation.com/wp-content/uploads/2023/03/gtddddddddddddhssssuddmfm.png'}/>
       </div>
-    </>
+    </div>
+  </>
   );
 };
-
 export default Login;
