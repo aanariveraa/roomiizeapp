1aadea504366ce5773a0732a1a22504914b387cc
Bryan Barrios <bryanbarrios@stg-edu-136-168-51-198.dyn.csub.edu>
2025-04-30 14:28:39 -0700

    home page fixes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,30 +4,29 @@ import { useUserAuth } from "../context/UserAuthContext";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../styles/Home.css"; 
-import welcomeAnim from "../components/welcome.json"; 
+import welcomeAnim from "../components/welcome.json";      // Sloth
+import welcomeTextAnim from "../components/welcomeText.json";  // New text animation
 import Lottie from "lottie-react";
 
-
 const Home = () => {
-  const { user, logOut } = useUserAuth();
+  const { logOut } = useUserAuth();
   const navigate = useNavigate();
 
-  // Handle Logoutf
   const handleLogout = async () => {
     try {
       await logOut();
-      navigate("/login"); // Redirect to login page
+      navigate("/login");
     } catch (error) {
       console.log("Logout Error:", error.message);
     }
   };
 
-  // sidebar for users to navigate between pages
   return (
     <div className="home-container">
       <div className="home-content-wrapper">
         <div className="left-content">
-          <h2>Welcome, {user.displayName}!</h2>
+          <Lottie animationData={welcomeTextAnim} loop={true} autoplay={true} className="welcome-lottie" />
+          <p className="welcome-subtext">Let's get started.</p>
         </div>
         <div className="right-lottie">
           <Lottie animationData={welcomeAnim} loop={true} autoplay={true} />
